aeb2bffabacc23705752f17e9301ba0cea36617a
Mitchell Tang <m3tang@gmail.com>
2025-02-26 12:48:02 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const cors = require("cors");
+const express = require("express");
+var morgan = require("morgan");
+const app = express();
+
+const PORT = 3001;
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/info", (req, res) => {
+  const count = persons.length;
+
+  const body = `
+  Phonebook has info for ${persons.length} people <br /> <br />
+  ${Date()}
+  `;
+  res.send(body);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  console.log(id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/persons/", (req, res) => {
+  const body = req.body;
+  if (!body.name) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  if (persons.some((p) => p.name === body.name)) {
+    return res.status(400).json({
+      error: "name exists",
+    });
+  }
+
+  if (persons.some((p) => p.number === body.number)) {
+    return res.status(400).json({
+      error: "number exists",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
